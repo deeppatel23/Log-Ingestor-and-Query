1efd9cbb3e9aeaa8b4bd54adbc0476e7a7a0e845
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-11-20 13:23:48 +0530

    added date logic

--- data-query-service/src/main/java/com/org/query/api/DataQueryResource.java
+++ data-query-service/src/main/java/com/org/query/api/DataQueryResource.java
@@ -4,9 +4,13 @@ import com.org.persistence.models.Logs;
 import com.org.persistence.repositories.LogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -18,16 +22,38 @@ public class DataQueryResource {
     LogRepository logRepository;
 
 
+
     @GetMapping("/query")
-    public List<Logs> findByParams(@RequestParam(required = false) String level,
+    public ResponseEntity<List<Logs>> findByParams(@RequestParam(required = false) String level,
                                    @RequestParam(required = false) String message,
                                    @RequestParam(required = false) String resourceId,
                                    @RequestParam(required = false) String traceId,
                                    @RequestParam(required = false) String spanId,
-                                   @RequestParam(required = false) String commit) {
+                                   @RequestParam(required = false) String commit,
+                                   @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) String fromTime,
+                                   @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) String endTime) {
 
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
-        return  logRepository.findByCustomParams(
-                resourceId, level, traceId, spanId, commit, message);
+            Timestamp fromTimeVal = (fromTime != null)
+                    ? Timestamp.valueOf(LocalDateTime.parse(fromTime, formatter))
+                    : null;
+
+            Timestamp endTimeVal = (endTime != null)
+                    ? Timestamp.valueOf(LocalDateTime.parse(endTime, formatter))
+                    : null;
+
+            List<Logs> logs = logRepository.findByCustomParams(
+                    resourceId, level, traceId, spanId, commit, message, fromTimeVal, endTimeVal);
+
+            return ResponseEntity.ok(logs);
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().build();
+        }
     }
+
+
 }
--- sql-data-persistence/src/main/java/com/org/persistence/repositories/LogRepository.java
+++ sql-data-persistence/src/main/java/com/org/persistence/repositories/LogRepository.java
@@ -15,17 +15,22 @@ public interface LogRepository extends JpaRepository<Logs, Integer> {
     Logs save(Logs logs);
 
     @Query("SELECT l FROM Logs l WHERE " +
-            "(:resourceId IS NULL OR l.resourceId = :resourceId) AND " +
-            "(:level IS NULL OR l.level = :level) AND " +
-            "(:traceId IS NULL OR l.traceId = :traceId) AND " +
-            "(:spanId IS NULL OR l.spanId = :spanId) AND " +
-            "(:commit IS NULL OR l.commit = :commit) AND " +
-            "(:message IS NULL OR l.message LIKE '%' || :message || '%')")
+            "(:resourceId IS NULL OR (l.resourceId IS NOT NULL AND l.resourceId = :resourceId)) AND " +
+            "(:level IS NULL OR (l.level IS NOT NULL AND l.level = :level)) AND " +
+            "(:traceId IS NULL OR (l.traceId IS NOT NULL AND l.traceId = :traceId)) AND " +
+            "(:spanId IS NULL OR (l.spanId IS NOT NULL AND l.spanId = :spanId)) AND " +
+            "(:commit IS NULL OR (l.commit IS NOT NULL AND l.commit = :commit)) AND " +
+            "(:message IS NULL OR (l.message IS NOT NULL AND l.message LIKE '%' || :message || '%')) AND " +
+            "(cast(:fromTime as timestamp) IS NULL OR (l.timestamp IS NOT NULL AND l.timestamp >= :fromTime)) AND " +
+            "(cast(:endTime as timestamp) IS NULL OR (l.timestamp IS NOT NULL AND l.timestamp <= :endTime))")
     List<Logs> findByCustomParams(
             @Param("resourceId") String resourceId,
             @Param("level") String level,
             @Param("traceId") String traceId,
             @Param("spanId") String spanId,
             @Param("commit") String commit,
-            @Param("message") String message);
+            @Param("message") String message,
+            @Param("fromTime") Timestamp fromTime,
+            @Param("endTime") Timestamp endTime);
+
 }
